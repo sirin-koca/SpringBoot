93bc266ea9795705c93561554d9459d522ced094
Sirin Koca <s182211@oslomet.no>
2022-04-26 14:48:16 +0200

    Spring initiate, readme.md, SpringPojoApplication.java updated

---
+++ src/main/java/oslomet/web/springpojo/SpringPojoApplication.java
@@ -0,0 +1,13 @@
+package oslomet.web.springpojo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPojoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringPojoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/oslomet/web/springpojo/SpringPojoApplicationTests.java
@@ -0,0 +1,13 @@
+package oslomet.web.springpojo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPojoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
